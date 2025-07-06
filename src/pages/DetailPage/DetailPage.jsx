ee776a102356ae5bdabb482e147a80f1c1d3b571
Jed Pham <haopv7@gmail.com>
2025-07-06 19:44:35 +0700

    upd detail page

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import newsApi from "../../api/NewsApi";
 import ArticleSkeleton from "../../components/Skeleton/SkeletonArticle/SkeletonArticle";
+import newsApi from "../../api/NewsApi";
 
 export default function DetailPage() {
   const { category, index } = useParams();
