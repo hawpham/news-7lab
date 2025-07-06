1c84dc5a458aae1ea9737b95e1a29ea755dbff1c
Jed Pham <haopv7@gmail.com>
2025-07-06 22:37:50 +0700

    update news api

--- src/api/gnewsApi.js
+++ src/api/gnewsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.NEWS_API_KEY;
+const API_KEY = import.meta.env.VITE_API_TOKEN;
 
 const gnewsApi = axios.create({
   baseURL: "https://gnews.io/api/v4",
